c9ba85d08cd948b83183514afa4910d9428706ac
Rupam Das <rejisterjack@gmail.com>
2022-04-27 23:56:19 +0530

    started js

---
+++ app.js
@@ -0,0 +1,14 @@
+// selectors
+
+let addCardBtn = document.getElementById("add-card-btn")
+const cards = document.getElementById("cards")
+// event listeners
+
+addCardBtn.addEventListener("click", addCard)
+
+// functions
+
+function addCard(event){
+  const card = document.createElement("div")
+  card.setAttribute()
+}
