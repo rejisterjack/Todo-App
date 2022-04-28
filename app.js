c217ae84198fefc03e7d76401d9178971de72dcc
Rupam Das <rejisterjack@gmail.com>
2022-04-28 13:17:38 +0530

    project completed

--- app.js
+++ app.js
@@ -1,202 +1,142 @@
-
-// // selectors
-
-// const showPopup = document.getElementById("show-popup")
-// const cards = document.getElementById("cards")
-// const popDiv = document.getElementsByClassName("popup")[0]
-// const finalPopDiv = document.getElementsByClassName("final-popup")[0]
-// const addTaskBtn = document.getElementById("add-task-btn")
-// const cancleTaskBtn = document.getElementById("cancle-task-btn")
-// const taskNameInput = document.getElementById("card-name")
-// const finalTaskNameInput = document.getElementById("final-card-name")
-// const addTaskCard = document.getElementById("add-task-card")
-// const delCard = document.getElementById("del-card")
-// const cardList = document.getElementById("card-list")
-// const finalAddTaskBtn = document.getElementById("final-add-task-btn")
-// const finalCancleTaskBtn = document.getElementById("final-cancle-task-btn")
-// // event listeners
-
-// showPopup.addEventListener("click", popup)
-// addTaskBtn.addEventListener("click", addTask)
-// cancleTaskBtn.addEventListener("click", cancleTask)
-// addTaskCard.addEventListener("click", addTaskName)
-// delCard.addEventListener("click",delThisCard)
-// finalAddTaskBtn.addEventListener("click",addLastTask)
-// finalCancleTaskBtn.addEventListener("click",lastCancle)
-
-// // functions
-// popDiv.style.visibility = 'hidden';
-// finalPopDiv.style.visibility = 'hidden';
-
-// function popup(event){
-//   document.getElementsByClassName("main-container")[0].style.filter = "blur(2px)"
-//   popDiv.style.visibility = 'visible';
-// }
-
-// function addTask(event){
-//   document.getElementsByClassName("main-container")[0].style.filter = "blur(0px)"
-//   popDiv.style.visibility = 'hidden';
-
-//   const card = document.createElement("div")
-//   card.setAttribute("class","card")
-  
-//   const taskName = document.createElement("h2")
-//   taskName.innerText=taskNameInput.value
-//   card.appendChild(taskName)
-
-//   const hrLine = document.createElement("hr")
-//   card.appendChild(hrLine)
-
-//   const taskNameList = document.createElement("ul")
-//   card.appendChild(taskNameList)
-
-//   const cardBtn = document.createElement("div")
-//   cardBtn.setAttribute("class","card-btn")
-
-//   const addTaskBtn = document.createElement("i")
-//   addTaskBtn.setAttribute("class","fa-solid fa-trash")
-//   cardBtn.appendChild(addTaskBtn)
-//   const delBtn = document.createElement("i")
-//   delBtn.setAttribute("class","fa-solid fa-circle-plus")
-//   cardBtn.appendChild(delBtn)
-
-//   card.appendChild(cardBtn)
-
-
-//   cards.appendChild(card)
-
-//   taskNameInput.value=""
-// }
-// function cancleTask(event){
-//   document.getElementsByClassName("main-container")[0].style.filter = "blur(0px)"
-//   popDiv.style.visibility = 'hidden';
-// }
-
-// function addTaskName(event){
-//   document.getElementsByClassName("main-container")[0].style.filter = "blur(2px)"
-//   finalPopDiv.style.visibility = 'visible';
-// }
-
-// function addLastTask(event){
-//   const listItem = document.createElement("li")
-  
-//   const finalTaskName = document.createElement("p")
-//   finalTaskName.innerText=finalTaskNameInput.value
-//   listItem.appendChild(finalTaskName)
-//   const markDone = document.createElement("i")
-//   markDone.setAttribute("class","fa-solid fa-check")
-//   listItem.appendChild(markDone)
-
-
-//   cardList.appendChild(listItem)
-
-//   finalTaskNameInput.value=""
-
-//   document.getElementsByClassName("main-container")[0].style.filter = "blur(0px)"
-//   finalPopDiv.style.visibility = 'hidden';
-// }
-
-// function lastCancle(event){
-//   document.getElementsByClassName("main-container")[0].style.filter = "blur(0px)"
-//   finalPopDiv.style.visibility = 'hidden';
-// }
-
-// function delThisCard(event){
-//   console.log("hrllo");
-// }
-
-
-// selector
-
-const popUp = document.getElementById("popup")
-const addTaskBtn = document.getElementById("add-task")
-const cardName = document.getElementById("card-name")
-const card = document.getElementById("card")
-
-// event listeners
-
-addTaskBtn.addEventListener("click", showPopup)
-popUp.addEventListener("click",popupInput)
-
-// functions
-
-popUp.style.visibility="hidden"
-// show popup
-function showPopup(event){
-  popUp.style.visibility="visible"
-  // document.getElementsByClassName("main-container")[0].style.filter="blur(3px)"
+var arr_of_obj = new Set();
+var value_id;
+var title_flag = false;
+var subtask = new Map;
+function modal(){
+    document.getElementById("modal-div").style.display = "block";
+};
+function addCard(){
+    var card_title = document.getElementById("modal-input-box").value;
+    createObj(card_title);
+    closeModal();
 }
-//popup input
-function popupInput(event){
-  const item = event.target
-  if(item.classList[0]==="add-tasklist"){
-    const cards = document.getElementById("cards")
+function closeModal(){
+    document.getElementById("modal-div").style.display = "none";
+}
+function createObj(title){
+    document.getElementById('empty-list').style.display = 'none'
+    var card_obj = {
+        title: title,
+        id: Date.now(),
+        subtask
+    };
+    arr_of_obj.add(card_obj);
+    createCard(card_obj.id);
+};
+function addList(){
+    var cloned_list_item = document.querySelector(".this-list-element").cloneNode(true);
+    var card_item = document.getElementById('modal-input-box-card').value;
+    console.log(value_id);
+    cloned_list_item.innerText =  card_item; 
+    cloned_list_item.style.display = "block";
+    cloned_list_item.setAttribute('id',`${Date.now()}`);
+    cloned_list_item.setAttribute('value',`${Date.now()}`);
+    cloned_list_item.setAttribute('style',"margin-left: 10px;");
+    var done_button = document.createElement('button');
+    done_button.setAttribute('id',`check-done-${Date.now()}`);
+    done_button.setAttribute('class','mark-as-done-class');
+    done_button.setAttribute('value',`${Date.now()}`);
+    done_button.setAttribute('onclick','completedTask(this.value)');
+    done_button.innerText = ' Mark Done';
+    done_button.setAttribute('style','font-size:15 px;cursor:pointer; height:18px; border-radius:10px;')
+    cloned_list_item.appendChild(done_button);
+    cloned_list_item.setAttribute('onClick',"completedTask(this.value)");
+     for(obj of arr_of_obj){
+        for(prop in obj){
+            if(obj.id == value_id){
+                obj.subtask.set(`${card_item}`,`${Date.now()}`);
+                break;
+            }
+        }
+    }
+    document.getElementById(`${value_id}`).getElementsByClassName('add-list-after-this')[0].appendChild(cloned_list_item).appendChild(done_button);
+    closeCardModal();
+}
+function closeCardModal(){
+    document.getElementById('modal-div-card').style.display = "none";
+}
+function addSubtask(val) {
+    document.getElementById("modal-div-card").style.display = "block";
+    value_id = val;
+};
+function deleteCard(val){
+    var delete_div = document.getElementById(`${val}`);
+    for(obj of arr_of_obj){
+        for(prop in obj){
+        if (obj.id==val)
+        arr_of_obj.delete(obj);
+        break;
+        }
+    }
+    delete_div.parentNode.removeChild(delete_div);
+    if(arr_of_obj.size==0){
+        document.getElementById('empty-list').style.display = 'block';
+    }
+    
+};
 
-    const card = document.createElement("div")
-    card.setAttribute("class","card")
-    card.setAttribute("id","card")
-
-    const taskListName = document.createElement("h2")
-    taskListName.innerText=cardName.value
-    card.appendChild(taskListName)
-    cardName.value=""
-
-    const hrLine = document.createElement("hr")
-    card.appendChild(hrLine)
-
-    const taskList = document.createElement("ul")
-    card.appendChild(taskList)
-
-    const cardBtnDiv = document.createElement("div")
-    cardBtnDiv.setAttribute("class","card-btn")
-
-    const cardDelBtn = document.createElement("i")
-    cardDelBtn.setAttribute("class","fa-solid fa-trash")
-    cardDelBtn.setAttribute("id","del-card")
-    cardBtnDiv.appendChild(cardDelBtn)
-
-    const addTaskBtn = document.createElement("i")
-    addTaskBtn.setAttribute("class","fa-solid fa-circle-plus")
-    addTaskBtn.setAttribute("id","add-task-card")
-    cardBtnDiv.appendChild(addTaskBtn)
-
-    card.appendChild(cardBtnDiv)
-
-    const finalPopup = document.createElement("div")
-    finalPopup.setAttribute("class","final-popup")
-
-    const finalPopupName = document.createElement("h2")
-    finalPopupName.innerText="add task here"
-    finalPopup.appendChild(finalPopupName)
-
-    const cardTaskName = document.createElement("input")
-    cardTaskName.setAttribute("class","final-task-name")
-    cardTaskName.setAttribute("id","final-task-name")
-    cardTaskName.setAttribute("type","text")
-    cardTaskName.setAttribute("value","")
-    finalPopup.appendChild(cardTaskName)
-
-    const finalPopupBtn = document.createElement("div")
-    finalPopupBtn.setAttribute("class","final-popup-btn")
-
-    const finalAddBtn = document.createElement("button")
-    finalAddBtn.setAttribute("id","final-add-task-btn")
-    finalAddBtn.innerText="add"
-    finalPopupBtn.appendChild(finalAddBtn)
-    const finalCancleBtn = document.createElement("button")
-    finalCancleBtn.setAttribute("id","final-cancle-task-btn")
-    finalCancleBtn.innerText="cancle"
-    finalPopupBtn.appendChild(finalCancleBtn)
-
-    finalPopup.appendChild(finalPopupBtn)
-    finalPopup.style.visibility="hidden"
-
-    card.appendChild(finalPopup)
-
-    cards.appendChild(card)
-    popUp.style.visibility="hidden"
-  }
-  if(item.classList[0]==="cancle-tasklist"){
-    popUp.style.visibility="hidden"
-  }
+function createCard(){
+    var first_card = document.querySelector('.card').cloneNode(true);
+    display(first_card);
+};
+function completedTask(value){
+    document.getElementById(`${value}`).style.textDecoration = 'line-through';
+    document.getElementById(`${value}`).style.color = '#112D4E';
+    document.getElementById(`check-done-${value}`).remove();
+}
+function display(card){
+    document.getElementById('empty-list').style.display = 'none'
+    arr_of_obj.forEach(element => {
+        card.id = element.id;
+        card.querySelector(".card-head").innerHTML = element.title;
+        card.querySelector(".card-head").setAttribute('value',`${element.id}`);
+        card.setAttribute("value",`${element.id}`);
+        card.setAttribute("display","block");
+        card.setAttribute("min-height","300px");
+        card.querySelector(".delete-button-in-card").setAttribute("value",`${element.id}`);
+        card.querySelector(".delete-button-in-card").setAttribute("onClick","deleteCard(this.value)");
+        card.querySelector(".add-button-in-card").setAttribute("value",`${element.id}`);
+        card.querySelector(".add-button-in-card").setAttribute("onClick","addSubtask(this.value)");    
+    });
+    if(title_flag)
+    card.style.display = 'none';
+    else
+    card.style.display = "block";
+    document.getElementById("outer-container").appendChild(card);
+}
+function headerFunc(val){
+    var card_header;
+    for(let ele of arr_of_obj){
+        for(let id in ele){
+            if(ele[id]==val){
+                card_header = ele.title;
+                break;
+            };
+        };
+    };
+    document.querySelector("#app-name").style.display = 'none';
+    document.querySelector("#add-button-text").style.display = 'none';
+    for(let ele of arr_of_obj){
+            if(ele.id==val){
+                document.getElementById(`${ele.id}`).style.display = 'block';
+            }
+            else {
+                document.getElementById(`${ele.id}`).style.display = 'none';
+            }
+    };
+    document.getElementById('card-dynamic-head').innerText = `${card_header}`;
+    document.getElementById('card-dynamic-head').style.display = 'flex'
+    document.getElementById('back-button').style.display = 'block'
+    title_flag = true;
+};
+function displayAll(){
+    title_flag = false;
+    document.querySelector("#app-name").style.display = 'block';
+    document.querySelector("#add-button-text").style.display = 'inline-block';
+    document.getElementById('back-button').style.display = 'none';
+    for(let ele of arr_of_obj){
+            document.getElementById(`${ele.id}`).style.display = 'block';
+    };
+    document.getElementById('card-dynamic-head').innerText = ``;
+    document.getElementById('card-dynamic-head').style.display = 'none';
 }
